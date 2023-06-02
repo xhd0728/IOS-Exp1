26223574d06e7510ff2ba5cc4f8a53fe0d525cfa
xhd0728 <xinhaidong0625@foxmail.com>
2023-06-02 09:14:02 +0800

    Initial Commit

---
+++ MyBMI/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyBMI
+//
+//  Created by Haidong Xin on 2023/6/2.
+//  Copyright © 2023 Haidong Xin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
