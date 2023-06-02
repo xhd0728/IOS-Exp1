c60a2ca542d3e486981e41c43424e0503bfe8ef3
xhd0728 <xinhaidong0625@foxmail.com>
2023-06-02 14:25:42 +0800

    init

--- MyBMI/main.m
+++ MyBMI/main.m
@@ -11,7 +11,22 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        double height=0, weight=0;
+        printf("\n请输入您的身高(m): \n>>");
+        scanf("%lf", &height);
+        printf("\n请输入您的体重(kg): \n>>");
+        scanf("%lf", &weight);
+        double BMI = weight / (height * height);
+        printf("\nBMI=%.2lf\n", BMI );
+        if(BMI < 18.5){
+            printf("\n您的体重过轻\n");
+        }
+        else if(BMI > 23.9){
+            printf("\n您的体重过重\n");
+        }
+        else{
+            printf("\n您的体重正常\n");
+        }
     }
     return 0;
 }
